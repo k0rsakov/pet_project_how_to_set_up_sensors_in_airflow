da18b771b65222054cd4fda41d6761136e923e90
k0rsakov <korsakov.iyu@gmail.com>
2025-11-18 09:03:55 +0300

    Изменён проект pet_project_how_to_set_up_sensors_in_airflow: - улучшены нейминги

--- dags/dm/dm_dag_with_sensor_and_another_cron.py
+++ dags/dm/dm_dag_with_sensor_and_another_cron.py
@@ -100,8 +100,8 @@ with DAG(
         task_id="start",
     )
 
-    sensor_ods = ExternalTaskSensor(
-        task_id="sensor_ods",
+    sensor_ods_dag_users_to_dwh_pg = ExternalTaskSensor(
+        task_id="sensor_ods_dag_users_to_dwh_pg",
         external_dag_id="ods_dag_users_to_dwh_pg",
         mode="reschedule",
         poke_interval=60,
@@ -109,8 +109,8 @@ with DAG(
         # execution_delta=pendulum.duration(hours=1),
     )
 
-    load_ods_layer = PythonOperator(
-        task_id="load_ods_layer",
+    load_dm_layer = PythonOperator(
+        task_id="load_dm_layer",
         python_callable=load_dm_layer,
     )
 
@@ -118,4 +118,4 @@ with DAG(
         task_id="end",
     )
 
-    start >> sensor_ods >> load_ods_layer >> end
+    start >> sensor_ods_dag_users_to_dwh_pg >> load_dm_layer >> end
